c9c8a77d9992f7d4640c4ebbf48f4e0eeb5251f3
Mojtaba-Rajaei <Mojtabaraj@outlook.com>
2024-02-17 10:30:49 +0330

    map work like this

---
+++ map.dart
@@ -0,0 +1,15 @@
+void main() {
+  // map
+  Map map = {"hello": 10};
+  print(map);
+  // add
+  map['who'] = "ali";
+  map['is'] = "verb";
+  print(map);
+  // asset
+  assert(map.isNotEmpty);
+  //
+  map.addAll({"hi": 2, "me": 0});
+  map.removeWhere((key, value) => key == "who");
+  print(map);
+}
